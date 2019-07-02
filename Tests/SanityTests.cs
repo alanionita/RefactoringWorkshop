6b51cb3560057e3897ec1a1aa1173900ae0d3836
Andrew Grant <andy.a.grant@gmail.com>
2019-07-02 05:51:48 +0100

    Added test project with sanity test to check tests are running.

---
+++ Tests/SanityTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
